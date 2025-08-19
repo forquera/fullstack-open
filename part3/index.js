a6988ce6003b8ca2327b09de8dd7ea340d8abbb1
Fernando <8602500+45Fernando@users.noreply.github.com>
2025-08-18 21:16:24 -0300

    part 3.2

--- part3/index.js
+++ part3/index.js
@@ -28,6 +28,15 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  const tamanio = persons.length;
+  const fecha = new Date().toLocaleString();
+
+  response.send(
+    `<p>Phonebook has info for ${tamanio} people</p> <p>${fecha}</p>`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
