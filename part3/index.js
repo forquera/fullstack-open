115670688f8277f2edb4d4e9ef802cafa2a2914d
Fernando <8602500+45Fernando@users.noreply.github.com>
2025-08-21 09:42:27 -0300

    part 3.3

--- part3/index.js
+++ part3/index.js
@@ -28,6 +28,18 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const person_id = Number(request.params.id);
+
+  const person = persons.find((person) => person.id === person_id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 app.get("/info", (request, response) => {
   const tamanio = persons.length;
   const fecha = new Date().toLocaleString();
